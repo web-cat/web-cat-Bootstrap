8a4ebfe7a4cbb45635a96aa2d6462bff5bd97480
s-edwards <edwards@cs.vt.edu>
2007-06-12 02:56:50 +0000

    Changed from an array to a map to speed up directory deletion with filtering.  Fixed a bug in the default exclusion list for the main application subsystem stub.

--- src/net/sf/webcat/WCServletAdaptor.java
+++ src/net/sf/webcat/WCServletAdaptor.java
@@ -412,7 +412,7 @@ public class WCServletAdaptor
         if ( System.getProperty( "os.name" ).indexOf( "Windows" ) >= 0 )
         {
             defaultDir = "C:/Apple";
-            File f = new File( defaultDir ); 
+            File f = new File( defaultDir );
             if ( !f.exists() || !f.isDirectory() )
             {
                 // Not found
@@ -422,7 +422,7 @@ public class WCServletAdaptor
         else if ( System.getProperty( "os.name" ).indexOf( "Mac" ) >= 0 )
         {
             defaultDir = "/System";
-            File f = new File( defaultDir ); 
+            File f = new File( defaultDir );
             if ( !f.exists() || !f.isDirectory() )
             {
                 // Not found
@@ -432,12 +432,12 @@ public class WCServletAdaptor
         else
         {
             defaultDir = "/opt/Apple";
-            File f = new File( defaultDir ); 
+            File f = new File( defaultDir );
             if ( !f.exists() || !f.isDirectory() )
             {
                 // Not found, so try /usr/local
                 defaultDir = "/usr/local/Apple";
-                f = new File( defaultDir ); 
+                f = new File( defaultDir );
                 if ( !f.exists() || !f.isDirectory() )
                 {
                     // Not found
@@ -658,7 +658,7 @@ public class WCServletAdaptor
             }
         }
 
-        String[] preserveOnUpdate = null;
+        Map preserveOnUpdate = null;
         {
             String preserveOnUpdateRaw =
                 updater.getProperty( "preserveOnUpdate" );
@@ -668,16 +668,18 @@ public class WCServletAdaptor
                     + "WEB-INF/web.xml,"
                     + "WEB-INF/update.properties,"
                     + "WEB-INF/Web-CAT.woa/Contents/Frameworks,"
+                    + "WEB-INF/Web-CAT.woa/Contents/Library,"
                     + "WEB-INF/Web-CAT.woa/configuration.properties,"
                     + "WEB-INF/pending-updates";
             }
             if ( preserveOnUpdateRaw != null )
             {
-                preserveOnUpdate = preserveOnUpdateRaw.split( ",\\s*" );
-                for ( int i = 0; i < preserveOnUpdate.length; i++ )
+                preserveOnUpdate = new HashMap();
+                for ( String entry : preserveOnUpdateRaw.split( ",\\s*" ) )
                 {
-                    preserveOnUpdate[i] = FileUtilities.normalizeFileName(
-                        new File( unpackDir, preserveOnUpdate[i] ) );
+                    String key = FileUtilities.normalizeFileName(
+                        new File( unpackDir, entry ) );
+                    preserveOnUpdate.put( key, key );
                 }
             }
         }
@@ -758,10 +760,10 @@ public class WCServletAdaptor
         {
             downloadUpdateIfNecessary( getUpdaterFor( subdirs[i] ) );
         }
-        
+
         // Now handle the application update, if available
         downloadUpdateIfNecessary( getUpdaterFor( mainBundle ) );
-        
+
         // Now check through existing subsystems and check for any required
         // subsystems that are not yet installed
         for ( Iterator i = subsystems.entrySet().iterator(); i.hasNext(); )
@@ -909,7 +911,7 @@ public class WCServletAdaptor
 
         return buffer.toString();
     }
-    
+
 
     // ----------------------------------------------------------
     /**
@@ -1010,7 +1012,7 @@ public class WCServletAdaptor
         }
 
         // Now create the updater for the main bundle
-        getUpdaterFor( mainBundle );        
+        getUpdaterFor( mainBundle );
     }
 
 
